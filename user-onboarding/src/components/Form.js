77430edbf5d35426266d06ef13b672e5328e78e3
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-02-27 14:23:44 -0800

    successfully implemented form validations using yup

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-function SignUpForm() {
+function SignUpForm(props) {
     return(
         <div className='sign-up-form'>
             <Form>
                 <label>Name: 
-                    <Field type='text' name='name' placeholder='Name'/>
+                    <Field type='text' name='name' placeholder='Name' onChange={props.handleChange} value={props.values.name}/>
                 </label>
+                {props.touched.name && props.errors.name && (<p className='errors'>{props.errors.name}</p>)}
                 <label>Email: 
-                    <Field type='email' name='email' placeholder='Email'/>
+                    <Field type='email' name='email' placeholder='Email' onChange={props.handleChange} value={props.values.email}/>
                 </label>
+                {props.touched.email && props.errors.email && (<p className='errors'>{props.errors.email}</p>)}
                 <label>Password: 
-                    <Field type='password' name='password' placeholder='Password' />
+                    <Field type='password' name='password' placeholder='Password' onChange={props.handleChange} value={props.values.password}/>
                 </label>
+                {props.touched.password && props.errors.password && (<p className='errors'>{props.errors.password}</p>)}
                 <label>Have you agreed to our Terms of Service?
-                    <Field type='checkbox' name='tos'
-                    //checked={values.tos}
-                    />
+                <Field type='checkbox' name='tos' onChange={props.handleChange} checked={props.values.tos}/>
                 </label>
+                {props.touched.tos && props.errors.tos && (<p className='errors'>{props.errors.tos}</p>)}
                 <button type='submit'>Submit</button>
             </Form>
         </div>
@@ -32,8 +35,15 @@ const FormikSignUpForm = withFormik({
             email: email || '',
             password: password || '',
             tos: tos || false
-        }
-    }
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().min(3).required('Name must be more than three characters.'),
+        email: Yup.string().email().required('You must provide a valid e-mail address'),
+        password: Yup.string().min(8).required('Password must exceed 8 characters'),
+        tos: Yup.boolean().oneOf([true], 'You must agree to our Terms of Service')
+    }),
+    
 })(SignUpForm);
 
 export default FormikSignUpForm;
